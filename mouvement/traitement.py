646f9108f8169111cbd3a9bf65b47f33a8970c53
tix06 <etixidor@hotmail.com>
2022-01-30 21:15:34 +0100

    notebook parabolique Em

--- mouvement/traitement.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd  
-import matplotlib.pyplot as plt
-data = pd.read_csv("coordonnees1.txt")
-data.Y = 400 - data.Y
-plt.axis([0, 600, 0, 400])
-plt.scatter(data.X,data.Y)
-plt.savefig('output.png')
